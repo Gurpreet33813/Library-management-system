95fb3dfeba63be400acc9b463cc1e38c42e32a06
Gurpreet <gurpreetpanwar64@gmail.com>
2026-04-22 14:29:12 +0530

    Add function to issue books from inventory

---
+++ 3.py
@@ -0,0 +1,9 @@
+#Issue
+def Issue_books():
+    name = input("Enter the book name: ")
+    if name in books:
+        books.remove(name)
+        Issued_books.append(name)
+        print("Books Issued")
+    else:
+        print("Book not available")
