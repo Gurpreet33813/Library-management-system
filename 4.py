cb2895128bdf7be2c613e1239cee90426cddf8a2
Gurpreet <gurpreetpanwar64@gmail.com>
2026-04-22 14:30:24 +0530

    Implement Return_books function to return a book

---
+++ 4.py
@@ -0,0 +1,8 @@
+#Return books
+def Return_books():
+    name = input("Enter the book name: ")
+    if name in Issued_books():
+        Issued_books.remove(name)
+        books.append(name)
+        print("Book returned")
+    else:
